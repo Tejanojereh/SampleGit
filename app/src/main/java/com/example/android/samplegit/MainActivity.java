15a96d5f15d53271608610d2c855d0619a7ddda6
Gwen Escarda <gwenescarda0521@gmail.com>
2018-11-29 17:50:27 +0800

    #sample push

--- app/src/main/java/com/example/android/samplegit/MainActivity.java
+++ app/src/main/java/com/example/android/samplegit/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Integer intd;
         String word = "Hi elisse";
+        String try1= "Try lng";
     }
 }
