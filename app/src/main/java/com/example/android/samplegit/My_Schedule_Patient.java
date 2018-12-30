094ce6b78f9c8127175ddbc37662fddee94e11ee
carms <carmeladevera56@gmail.com>
2018-12-30 22:29:29 +0800

    Merge branch 'master' of https://github.com/Tejanojereh/SampleGit
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/java/com/example/android/samplegit/MainActivity.java

--- app/src/main/java/com/example/android/samplegit/My_Schedule_Patient.java
+++ app/src/main/java/com/example/android/samplegit/My_Schedule_Patient.java
@@ -3,7 +3,12 @@ package com.example.android.samplegit;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.*;
-
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -18,6 +23,7 @@ public class My_Schedule_Patient extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_appointment_tbpartner);
         appt_date=findViewById(R.id.appointment_date);
+
 
 
     }
