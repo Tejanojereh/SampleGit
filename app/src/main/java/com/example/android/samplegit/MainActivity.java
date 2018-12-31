5f2456175604e907537b623342beca6ad0963b86
ELISSE EUSEBIO <eeusebio@live.mcl.edu.ph>
2018-12-31 16:26:17 +0800

    #modified - added code for log in (incomplete need to connect to db) #modified - fix error for ArrayAdapter sa sputum exam (incomplete need to connect to db)

--- app/src/main/java/com/example/android/samplegit/MainActivity.java
+++ app/src/main/java/com/example/android/samplegit/MainActivity.java
@@ -1,16 +1,54 @@
 package com.example.android.samplegit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText txtUsername, txtPassword;
+    ImageButton imgBtnSignIn, imgBtnForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Integer intd;
-        String word = "Hi elisse";
-        String chicka= "Elisse may kwekwento ko sayo..... seryoso hahhaa";
+        InstantiateControl();
     }
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, PatientInformationModule.class);
+        switch (v.getId())
+        {
+            //sign in
+            case R.id.imageButton:
+            {
+                Toast.makeText(this, "Sign In", Toast.LENGTH_SHORT).show();
+                //access db
+            }break;
+
+            //forgot password
+            case R.id.imageButton2:
+            {
+                //redirect?? ko muna sa add sputum exam.. peace
+                Toast.makeText(this, "Forgot Pword", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+
+            }break;
+        }
+    }
+
+    private void InstantiateControl(){
+        txtUsername = (EditText)findViewById(R.id.txtUsername);
+        txtPassword = (EditText)findViewById(R.id.txtPassword);
+        imgBtnSignIn = (ImageButton)findViewById(R.id.imageButton);
+        imgBtnForgotPassword = (ImageButton)findViewById(R.id.imageButton2);
+
+        imgBtnForgotPassword.setOnClickListener(this);
+        imgBtnSignIn.setOnClickListener(this);
+    }
+
+
 }
