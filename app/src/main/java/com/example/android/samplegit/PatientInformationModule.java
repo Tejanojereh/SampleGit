dbbd5ce715b048edf60fcd79b1eccc251d542c94
ELISSE EUSEBIO <eeusebio@live.mcl.edu.ph>
2019-01-08 01:06:21 +0800

    #Update

--- app/src/main/java/com/example/android/samplegit/PatientInformationModule.java
+++ app/src/main/java/com/example/android/samplegit/PatientInformationModule.java
@@ -8,6 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.*;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
@@ -15,6 +21,9 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -35,6 +44,8 @@ public class PatientInformationModule extends AppCompatActivity implements View.
     HttpClient httpclient; HttpResponse httpresponse; HttpPost httppost;
     StringBuffer stringbuffer = null; InputStream inputstream; BufferedReader bufferedreader;
     List<NameValuePair> nameValuePairs;
+
+    private JSONArray result;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -82,8 +93,48 @@ public class PatientInformationModule extends AppCompatActivity implements View.
         spinnerDiagnosis.setAdapter(arrayAdapterDiagnose);
 
         //TBPatient Spinner Populate
-        new ExecuteTask(this).execute();
+        //new ExecuteTask(this).execute();
     }
+
+    //Alternative way to fetch data while webservice is not working
+    /*public void GetTBPatient() {
+        StringRequest request = new StringRequest("http://localhost/TBCareService/retrieveAssignedPatient.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject j = null;
+                        try {
+                            j = new JSONObject(response);
+                            result = j.getJSONArray(Config.JSON_ARRAY);
+                            getPatient(result);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+    }
+
+
+    private void getPatient(JSONArray j){
+        ArrayList<String> patient = new ArrayList<String>();
+        for(int i=0;i<j.length();i++){
+            try {
+                JSONObject json = j.getJSONObject(i);
+                patient.add(json.getString(Config.TAG_USERNAME));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        spinnerPatient.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, patient));
+    }*/
 
     public String[] ReadResponse() {
         try {
@@ -125,20 +176,22 @@ public class PatientInformationModule extends AppCompatActivity implements View.
             String[] patientList;
             nameValuePairs = new ArrayList<NameValuePair>();
             nameValuePairs.add(new BasicNameValuePair("TP_ID", "TP000001"));
-            httppost = new HttpPost("http://192.168.43.1/TBCareService/retrieveAssignedPatient.php");
+            httppost = new HttpPost("http://localhost/TBCareService/retrieveAssignedPatient.php");
             try {
                 httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
                 httpresponse = httpclient.execute(httppost);
 
-                if((patientList = ReadResponse()) != null) {
+                if ((patientList = ReadResponse()) != null) {
                     arrayAdapterPatient = new ArrayAdapter<CharSequence>(context, android.R.layout.simple_spinner_item, patientList);
                     arrayAdapterPatient.setDropDownViewResource(android.R.layout.simple_spinner_item);
                     spinnerPatient.setAdapter(arrayAdapterPatient);
                 }
 
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            catch (UnsupportedEncodingException e) { e.printStackTrace(); }
-            catch (IOException e) { e.printStackTrace(); }
             return null;
         }
     }
