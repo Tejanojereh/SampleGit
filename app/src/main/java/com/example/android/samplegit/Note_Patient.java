7bfc9fc482df8df4c6c4ade2aeaad0e8e4a0616e
carms <carmeladevera56@gmail.com>
2019-01-15 10:21:22 +0800

    Merge branch 'master' of https://github.com/Tejanojereh/SampleGit
    # Conflicts:
    #	app/src/main/java/com/example/android/samplegit/MainActivity.java
    #	app/src/main/java/com/example/android/samplegit/My_Schedule_Patient.java

--- app/src/main/java/com/example/android/samplegit/Note_Patient.java
+++ app/src/main/java/com/example/android/samplegit/Note_Patient.java
@@ -1,4 +1,16 @@
 package com.example.android.samplegit;
 
-public class Note_Patient {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Note_Patient extends AppCompatActivity
+{
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+
+
+    }
 }
