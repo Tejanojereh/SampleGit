60e34d8cdaf2a881d8b1173973d0cd2a90acab37
carms <carmeladevera56@gmail.com>
2019-01-15 18:29:22 +0800

    Merge branch 'master' of https://github.com/Tejanojereh/SampleGit
    # Conflicts:
    #	app/src/main/java/com/example/android/samplegit/MainActivity.java
    #	app/src/main/java/com/example/android/samplegit/My_Schedule_Patient.java

--- app/src/main/java/com/example/android/samplegit/Note_Patient.java
+++ app/src/main/java/com/example/android/samplegit/Note_Patient.java
@@ -1,9 +1,12 @@
 package com.example.android.samplegit;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
@@ -25,15 +28,89 @@ public class Note_Patient extends AppCompatActivity {
 
 
     EditText note;
-
+    ImageButton back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
         note = (EditText) findViewById(R.id.txt_note);
-       new WebService().execute();
+        back = (ImageButton) findViewById(R.id.btn_back);
+
+
+        //updates Patient_note table and returns to My Schedule view
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Note_Patient.this, My_Schedule_Patient.class );
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+
+        new WebService().execute();
 
     }
+
+    class WebSer_Back extends AsyncTask {
+        @Override
+        protected Void doInBackground(Object... objects) {
+            byte[] data;
+            HttpPost httpPost;
+            StringBuffer buffer;
+            HttpResponse response;
+            HttpClient httpclient;
+            InputStream inputStream;
+            final String message;
+
+            List<NameValuePair> nameValuePairs;
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("ID", "TB000001"));
+            nameValuePairs.add(new BasicNameValuePair("NOTES", note.getText().toString()));
+            try {
+                httpclient = new DefaultHttpClient();
+                httpPost = new HttpPost("http://192.168.137.1/updatenotes.php");
+
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                response = httpclient.execute(httpPost);
+                inputStream = response.getEntity().getContent();
+
+                data = new byte[256];
+                buffer = new StringBuffer();
+                int len = 0;
+
+                while (-1 != (len = inputStream.read(data))) {
+                    buffer.append(new String(data, 0, len));
+
+
+                }
+                String s = buffer.toString();
+                inputStream.close();
+
+
+
+            } catch (final Exception e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(Note_Patient.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+            return null;
+        }
+
+
+    }
+
+
+
+
+
 
     class WebService extends AsyncTask {
         @Override
