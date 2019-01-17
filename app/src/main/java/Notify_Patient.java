09dfee640cf4b4e2606d1202b39221fb96fd33d3
carms <carmeladevera56@gmail.com>
2019-01-17 13:53:33 +0800

    Merge branch 'master' of https://github.com/Tejanojereh/SampleGit
    # Conflicts:
    #	app/src/main/java/com/example/android/samplegit/MainActivity.java
    #	app/src/main/java/com/example/android/samplegit/My_Schedule_Patient.java

---
+++ app/src/main/java/Notify_Patient.java
@@ -0,0 +1,13 @@
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+
+public class Notify_Patient extends AppCompatActivity {
+
+AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+AlertDialog alertDialog = alertDialogBuilder.create();
+
+
+
+}
